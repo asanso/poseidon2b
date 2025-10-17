1e5ba7014ec329ab9432fa5a7b3a8b94d05cd5c2
Anjan Roy <hello@itzmeanjan.in>
2025-10-17 23:00:53 +0530

    Prepend internal functions and state variables with `_`
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- ref_impl/poseidon2b/poseidon2b.py
+++ ref_impl/poseidon2b/poseidon2b.py
@@ -5,45 +5,45 @@ import numpy as np
 
 class Poseidon2b:
     def __init__(self, params: Poseidon2bPermutationParameters) -> None:
-        self.ROUND_CONSTANTS = params.generate_round_constants()
-        self.FULL_ROUND_MATRIX = params.generate_full_round_matrix()
-        self.PARTIAL_ROUND_MATRIX = params.generate_partial_round_matrix()
+        self._ROUND_CONSTANTS = params.generate_round_constants()
+        self._FULL_ROUND_MATRIX = params.generate_full_round_matrix()
+        self._PARTIAL_ROUND_MATRIX = params.generate_partial_round_matrix()
 
-        self.GF = gf.GF(2**params.gf_degree)
-        self.params = params
+        self._GF = gf.GF(2**params.gf_degree)
+        self._params = params
 
-    def AddRoundCons(self, state: gf.FieldArray, ridx: int) -> gf.FieldArray:
-        return state + self.ROUND_CONSTANTS[ridx]
+    def _AddRoundCons(self, state: gf.FieldArray, ridx: int) -> gf.FieldArray:
+        return state + self._ROUND_CONSTANTS[ridx]
 
-    def SubWordsRF(self, state: gf.FieldArray) -> gf.FieldArray:
-        return self.GF(list(map(lambda x: x**self.params.alpha, state)))
+    def _SubWordsRF(self, state: gf.FieldArray) -> gf.FieldArray:
+        return self._GF(list(map(lambda x: x**self._params.alpha, state)))
 
-    def SubWordsRP(self, state: gf.FieldArray) -> gf.FieldArray:
-        state[0] = state[0] ** self.params.alpha
+    def _SubWordsRP(self, state: gf.FieldArray) -> gf.FieldArray:
+        state[0] = state[0] ** self._params.alpha
         return state
 
-    def RoundFull(self, state: gf.FieldArray, ridx: int) -> gf.FieldArray:
-        state = self.AddRoundCons(state, ridx)
-        state = self.SubWordsRF(state)
-        state = np.matmul(self.FULL_ROUND_MATRIX, state)
+    def _RoundFull(self, state: gf.FieldArray, ridx: int) -> gf.FieldArray:
+        state = self._AddRoundCons(state, ridx)
+        state = self._SubWordsRF(state)
+        state = np.matmul(self._FULL_ROUND_MATRIX, state)
 
         return state
 
-    def RoundPartial(self, state: gf.FieldArray, ridx: int) -> gf.FieldArray:
-        state = self.AddRoundCons(state, ridx)
-        state = self.SubWordsRP(state)
-        state = np.matmul(self.PARTIAL_ROUND_MATRIX, state)
+    def _RoundPartial(self, state: gf.FieldArray, ridx: int) -> gf.FieldArray:
+        state = self._AddRoundCons(state, ridx)
+        state = self._SubWordsRP(state)
+        state = np.matmul(self._PARTIAL_ROUND_MATRIX, state)
 
         return state
 
     def permute(self, state: gf.FieldArray) -> gf.FieldArray:
-        state = np.matmul(self.FULL_ROUND_MATRIX, state)
+        state = np.matmul(self._FULL_ROUND_MATRIX, state)
 
-        for ridx in range(self.params.num_total_rounds()):
-            if self.params.is_full_round(ridx):  # full rounds
-                state = self.RoundFull(state, ridx)
+        for ridx in range(self._params.num_total_rounds()):
+            if self._params.is_full_round(ridx):  # full rounds
+                state = self._RoundFull(state, ridx)
             else:  # partial rounds
-                state = self.RoundPartial(state, ridx)
+                state = self._RoundPartial(state, ridx)
 
         return state
 
